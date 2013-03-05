3e36a0e85e2256ae9870eaad6e44c5779658d3e5
Geert Barentsen <geert@barentsen.be>
2013-03-05 18:51:11 +0000

    Made PEP8 compatible

--- examples/C2011L4.py
+++ examples/C2011L4.py
@@ -1,40 +1,50 @@
+"""
+Show the visibility of comet C/2011 L4 (PANSTARRS)
+
+To make a movie out of the results, do:
+cat $(cat list.txt) | ffmpeg -s 1018x720 -y -an -r 10 -f image2pipe -vcodec png -i - movie.mp4
+"""
 import sys
 sys.path.append("../")
 import logging
 import visibility
-import ephem
 import datetime
+from matplotlib import pyplot as plt
 from multiprocessing import Pool
 
 # xephem line obtained from MPC
 target = 'C/2011 L4 (PANSTARRS),h,03/10.1696/2013,84.2072,65.6658,333.6517,1.000028,0.301542,2000,5.5,4.0'
 
+
 def do_plot(date):
     logging.info(date)
 
-    step = 0.2
+    step = 1.2
     m = visibility.Map(lonstep=step, latstep=step)
     m.set_target_xephem(target)
     m.render(date)
 
     m.figure.text(.03, .93, 'Visibility of comet Pan-STARRS', fontsize=22, ha='left')
     m.figure.text(.03, .87, '%d %s at %s UTC' % (
-                                date.day, 
-                                date.strftime('%B %Y'), 
-                                date.strftime('%H:%M'))
-                            , fontsize=22, ha='left')
+                  date.day,
+                  date.strftime('%B %Y'),
+                  date.strftime('%H:%M')),
+                  fontsize=22, ha='left')
     m.figure.savefig('tmp/C2011L4-%s.png' % date.strftime('%Y%m%dT%H%M'), dpi=150)
+    # Avoid memory leak
+    m.figure.clf()
+    plt.close()
 
 
 if __name__ == '__main__':
     dates = []
     stepsize = datetime.timedelta(hours=0.5)
-    mydate = datetime.datetime(2013, 3, 5)
-    for i in range(24*60):
-        dates.append( mydate )
+    mydate = datetime.datetime(2013, 3, 23)
+    for i in range(48*10):
+        dates.append(mydate)
         mydate += stepsize
 
-    p = Pool(processes=4)
-    p.map(do_plot, dates)
-
-    #do_plot(mydate)
+    #p = Pool(processes=7)
+    #p.map(do_plot, dates)
+    mydate = datetime.datetime(2013, 3, 23)
+    do_plot(mydate)
--- visibility.py
+++ visibility.py
@@ -1,6 +1,7 @@
 """
-Plot a global visibility map of a celestial object
+Plot a global visibility map for a celestial object
 Author: Geert Barentsen
+
 """
 import matplotlib as mpl
 mpl.use('Agg')
@@ -13,14 +14,14 @@ import ephem
 class Map():
     """Computes a visibility map"""
 
-    def __init__(self, lon1=-170., lon2=190., 
+    def __init__(self, lon1=-170., lon2=190.,
                  lat1=-57., lat2=74., lonstep=1.0, latstep=1.0):
         """
         Arguments
         date: Time (UTC)
         ra/dec: Equatorial coordinates of the celestial object (degrees)
-        lon1/lon2: Longitudes at the left and right edges of the map (degrees east)
-        lat1/lat2: Latitudes at the bottom and top edges of the map (degrees north)
+        lon1/lon2: Longitudes of the left/right edges of the map (degrees east)
+        lat1/lat2: Latitudes of the bottom/top edges of the map (degrees north)
         """
         self.lon1, self.lon2 = lon1, lon2
         self.lat1, self.lat2 = lat1, lat2
@@ -28,19 +29,21 @@ class Map():
         self.longitudes = np.arange(lon1, lon2+lonstep, lonstep)
 
     def set_target(self, ra, dec):
+        """Define the target by giving fixed ra/dec coordinates"""
         self.target = ephem.FixedBody()
         self.target._ra = ephem.degrees('%s' % ra)
         self.target._dec = ephem.degrees('%s' % dec)
 
     def set_target_xephem(self, xephem):
+        """Define the target using an xephem ephemeris line"""
         self.target = ephem.readdb(xephem)
 
     def _compute_elevations(self, date):
-        """Compute the object and the solar elevation across Earth using PyEphem"""
+        """Compute the object and solar elevation across Earth using PyEphem"""
         # Sun Pyephem object
         sun = ephem.Sun()
 
-        result = [] # list containing radiant altitudes across the world (or NaN if daylight)
+        result = []  # list containing radiant altitudes (or NaN if daylight)
         for lat in self.latitudes:
             for lon in self.longitudes:
                 # Create a PyEphem "Observer" object at the given lon/lat
@@ -48,20 +51,20 @@ class Map():
                 observer.lat = ephem.degrees('%s' % lat)
                 observer.long = ephem.degrees('%s' % lon)
                 observer.date = date
-                
+
                 # Compute target and solar elevation
                 self.target.compute(observer)
-                target_alt = np.degrees(self.target.alt) 
+                target_alt = np.degrees(self.target.alt)
                 sun.compute(observer)
-                sun_alt = np.degrees(sun.alt) 
-                
+                sun_alt = np.degrees(sun.alt)
+
                 if sun_alt > -6 or target_alt < 0:
-                    result.append( np.NaN )
+                    result.append(np.NaN)
                 else:
-                    result.append( target_alt )
+                    result.append(target_alt)
 
         # Return the result as a matrix
-        return np.array(result).reshape(len(self.latitudes), 
+        return np.array(result).reshape(len(self.latitudes),
                                         len(self.longitudes))
 
     def render(self, date, mode='web'):
@@ -69,35 +72,37 @@ class Map():
 
         """ Create the plot """
         self.figure = plt.figure(figsize=(8.485, 6))
-        self.figure.subplots_adjust(0.03, 0.00, 0.97, 0.95, 
+        self.figure.subplots_adjust(0.03, 0.00, 0.97, 0.95,
                                     hspace=0.0, wspace=0.0)
 
         # Show earth with Mercator projection
-        ax = Basemap(projection='merc', 
-                    llcrnrlon=self.lon1, llcrnrlat=self.lat1, 
-                    urcrnrlon=self.lon2, urcrnrlat=self.lat2, 
-                    resolution="c", fix_aspect=False)
+        ax = Basemap(projection='merc',
+                     llcrnrlon=self.lon1, llcrnrlat=self.lat1,
+                     urcrnrlon=self.lon2, urcrnrlat=self.lat2,
+                     resolution="c", fix_aspect=False)
         ax.drawcoastlines()
 
-
         if mode == 'pub':
-            cdict = {'red'  :    ((0., .75, .75), (1., 0.05, 0.05)), 
-                     'green':  ((0., .75, .75), (1., 0.05, 0.05)), 
-                     'blue' :  ((0., .75, .75), (1., 0.05, 0.05))}
+            cdict = {'red':    ((0., .75, .75), (1., 0.05, 0.05)),
+                     'green':  ((0., .75, .75), (1., 0.05, 0.05)),
+                     'blue':  ((0., .75, .75), (1., 0.05, 0.05))}
         else:
-            cdict = {'red'  :  ((0., 1., 1.), (1., .0, 0.)), 
-                 'green':  ((0., 1., 1.), (1., 1., 1.)), 
-                 'blue' :  ((0., 0., 0.), (1., 0., 0.))}
+            cdict = {'red':  ((0., 1., 1.), (1., .0, 0.)),
+                     'green':  ((0., 1., 1.), (1., 1., 1.)),
+                     'blue':  ((0., 0., 0.), (1., 0., 0.))}
 
-        my_cmap = mpl.colors.LinearSegmentedColormap('my_colormap', cdict, 1024)
+        my_cmap = mpl.colors.LinearSegmentedColormap('my_colormap',
+                                                     cdict, 1024)
         im = ax.imshow(elevations, cmap=my_cmap, vmin=0, vmax=90)
-                
-        cb = self.figure.colorbar(im, orientation='horizontal', pad=0.02, shrink=0.95, 
-                            ticks=[0,30,60,90], aspect=50, format=u'%.0f\N{DEGREE SIGN}')
-        cb.ax.set_xlabel('Elevation above the horizon (when dark)', fontsize=18)
+
+        cb = self.figure.colorbar(im, orientation='horizontal',
+                                  pad=0.02, shrink=0.95,
+                                  ticks=[0, 30, 60, 90], aspect=50,
+                                  format=u'%.0f\N{DEGREE SIGN}')
+        cb.ax.set_xlabel('Elevation above the horizon (when dark)',
+                         fontsize=18)
         cl = plt.getp(cb.ax, 'xmajorticklabels')
         plt.setp(cl, fontsize=16)
-
 
 
 if __name__ == '__main__':
